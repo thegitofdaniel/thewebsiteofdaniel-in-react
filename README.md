b2fe91401cb451a6a3be9a12e74594eb5f57b56d
Daniel Ruiz <43095611+thegitofdaniel@users.noreply.github.com>
2024-05-13 18:59:52 +0000

    Lint

--- README.md
+++ README.md
@@ -62,9 +62,11 @@ Contributions are welcome! If you find any bugs, typos, or have suggestions for 
 This project is licensed under the MIT License. See the LICENSE file for details.
 
 ## Setting up Strato
+
 This website is deployed to Strato. [Here](https://www.schminkel.de/articles/2022-04-04_github-pages-with-strato) you will find instructions on how to set up GitHub Pages and Strato.
 
 ## GitHub Pages
+
 This website is deployed with GitHub Pages. [Here](https://blog.logrocket.com/deploying-react-apps-github-pages/) you will find instructions on how to do this deployment.
 
 ## Thank-You
--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -8,9 +8,26 @@ function Contact() {
                     <h1>Let&apos;s Talk</h1>
                     <p>I will get back to you as soon as possible.</p>
                     <form action="https://formspree.io/f/xoqgwwlw" method="POST">
-                        <input type="text" name="Name" placeholder="Name" autocomplete="name" required="" />
-                        <input type="email" name="Email" placeholder="Email" autocomplete="email" required="" />
-                        <textarea name="Message" placeholder="Message" autocomplete="off" required=""></textarea>
+                        <input
+                            type="text"
+                            name="Name"
+                            placeholder="Name"
+                            autocomplete="name"
+                            required=""
+                        />
+                        <input
+                            type="email"
+                            name="Email"
+                            placeholder="Email"
+                            autocomplete="email"
+                            required=""
+                        />
+                        <textarea
+                            name="Message"
+                            placeholder="Message"
+                            autocomplete="off"
+                            required=""
+                        ></textarea>
                         <input type="submit" value="Submit" className="btn btn-primary" />
                     </form>
                 </div>
