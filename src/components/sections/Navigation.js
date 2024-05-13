bf0e4c47ea334d07af1e544c1536e359f3ca7970
Daniel Ruiz <43095611+thegitofdaniel@users.noreply.github.com>
2024-05-13 20:21:13 +0000

    Revert "Merge branch 'feature/add_react_files' of https://github.com/thegitofdaniel/thewebsiteofdaniel-in-react into feature/add_react_files"
    This reverts commit ac75d8b456ef9041c28bea04820aa53839aea14e, reversing
    changes made to ae06f52da57c6658edd19edc915ebb0a09450988.

--- src/components/sections/Navigation.js
+++ src/components/sections/Navigation.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
 import useScrollShadow from './../../functions/useScrollShadow';
 import MenuButton from './../MenuButton';
 
 function Navigation() {
     useScrollShadow('nav', 'show-box-shadow');
-    const location = useLocation();
 
     return (
         <nav>
             <div className="container">
-                <Link to={location.pathname}>
+                <a href="#">
                     <h3>Daniel Ruiz</h3>
-                </Link>
+                </a>
                 <MenuButton />
             </div>
         </nav>
