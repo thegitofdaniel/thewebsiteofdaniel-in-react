3da1178c168504349457afb64f670365ceb857b1
Daniel Ruiz <43095611+thegitofdaniel@users.noreply.github.com>
2024-05-28 20:57:17 +0000

    Refactor paragraph with list

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,59 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import dalle_upward_arrow from './../../images/dalle-upward-arrow.png';
 
-const paragraph1 = (
-    <p>
-        I have developed awesome projects to clients in Brazil, Canada, Germany, Mexico, The
-        Netherlands, and Switzerland. By assessing the root cause of their challenges and choosing
-        the best technology, I&apos;ve unlocked value and positive change. I&apos;ve made a tangible
-        impact on both my clients&apos; success and the broader landscape of innovation. It&apos;s a
-        journey fueled by passion and purpose, and I&apos;m excited to continue making strides in
-        this ever-evolving field.
-    </p>
-);
-
-const paragraph2 = (
-    <p>
-        Since 2020, I&apos;ve played a pivotal role in building greenfield Data Platforms to enable
-        large-caps on AIML and maximize the value derived from data. My contributions include
-        developing models for Energy and Gas consumption using Azure Machine Learning, utilizing
-        state-of-the-art algorithms to input missing data, establishing MLOPS processes for model
-        deployment, implementing robust data pipelines, and introducing agile methodologies to
-        strengthen team collaboration.
-    </p>
-);
-
-const paragraph3 = (
-    <p>
-        In addition to my core responsibilities, I&apos;ve also been actively involved in fostering
-        a culture of continuous learning and innovation within the teams I&apos;ve worked with. This
-        has included organizing knowledge-sharing sessions, conducting workshops on emerging
-        technologies and best practices, and mentoring junior team members to help them grow their
-        skills and expertise in data science and machine learning. Furthermore, I&apos;ve been
-        instrumental in fostering strong cross-functional relationships between data science,
-        engineering, and business stakeholders, ensuring alignment of objectives and effective
-        communication channels throughout project life-cycles. By championing a collaborative and
-        growth-oriented environment, I&apos;ve helped foster a sense of ownership and accountability
-        among team members, resulting in greater efficiency and success in achieving our project
-        goals.
-    </p>
-);
+const paragraphs = [
+    `I have developed awesome projects to clients in Brazil, Canada, Germany, Mexico, The Netherlands, and Switzerland. By assessing the root cause of their challenges and choosing the best technology, I've unlocked value and positive change. I've made a tangible impact on both my clients' success and the broader landscape of innovation. It's a journey fueled by passion and purpose, and I'm excited to continue making strides in this ever-evolving field.`,
+    `Since 2020, I've played a pivotal role in building greenfield Data Platforms to enable large-caps on AIML and maximize the value derived from data. My contributions include developing models for Energy and Gas consumption using Azure Machine Learning, utilizing state-of-the-art algorithms to input missing data, establishing MLOPS processes for model deployment, implementing robust data pipelines, and introducing agile methodologies to strengthen team collaboration.`,
+    `In addition to my core responsibilities, I've also been actively involved in fostering a culture of continuous learning and innovation within the teams I've worked with. This has included organizing knowledge-sharing sessions, conducting workshops on emerging technologies and best practices, and mentoring junior team members to help them grow their skills and expertise in data science and machine learning. Furthermore, I've been instrumental in fostering strong cross-functional relationships between data science, engineering, and business stakeholders, ensuring alignment of objectives and effective communication channels throughout project life-cycles. By championing a collaborative and growth-oriented environment, I've helped foster a sense of ownership and accountability among team members, resulting in greater efficiency and success in achieving our project goals.`
+];
 
 function About() {
     const [isExpanded, setIsExpanded] = useState(false);
 
-    useEffect(() => {
-        const toggleContent = () => {
-            setIsExpanded((prev) => !prev);
-        };
-
-        const readMore = document.querySelector('.read-more');
-        readMore.addEventListener('click', toggleContent);
-
-        return () => {
-            readMore.removeEventListener('click', toggleContent);
-        };
-    }, []);
+    const toggleContent = () => {
+        setIsExpanded((prev) => !prev);
+    };
 
     return (
         <section className="about" id="about">
@@ -67,14 +26,15 @@ function About() {
                 <div className="flex-container">
                     <div className="flex-item">
                         <div className="info">
-                            {paragraph1}
-                            <div
-                                className={`read-more-content ${isExpanded ? 'show-content' : ''}`}
-                            >
-                                {paragraph2}
-                                {paragraph3}
+                            <p>{paragraphs[0]}</p>
+                            <div className={`read-more-content ${isExpanded ? 'show-content' : ''}`}>
+                                {paragraphs.slice(1).map((paragraph, index) => (
+                                    <p key={index}>{paragraph}</p>
+                                ))}
                             </div>
-                            <a className="read-more">{isExpanded ? 'Show Less' : 'Show More'}</a>
+                            <a className="read-more" onClick={toggleContent}>
+                                {isExpanded ? 'Show Less' : 'Show More'}
+                            </a>
                         </div>
                     </div>
                 </div>
