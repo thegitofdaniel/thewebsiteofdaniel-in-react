19c142cb14ff6a701aa96cbc35fe88b88ccfe132
Daniel Ruiz <43095611+thegitofdaniel@users.noreply.github.com>
2024-05-28 20:50:28 +0000

    Refactor projects as list

--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -1,7 +1,46 @@
 import React from 'react';
-import pypi_interesting from './../../images/pypi-interesting.png';
-import retirement_in_italy from './../../images/retirement-in-italy.png';
-import the_website_of_daniel from './../../images/thewebsiteofdaniel.png';
+
+const projects = [
+    {
+        thumbnail: require('./../../images/pypi-interesting.png'),
+        altText: 'PyPI Interesting',
+        title: 'interesting',
+        description: `The interesting Python package is a specialized toolkit for dealing with
+            Brazilian fixed income titles. It provides functionalities to build,
+            analyze, visualize, and valuate various types of fixed income
+            instruments and portfolios made thereof.`,
+        actions: [
+            { href: 'https://github.com/thegitofdaniel/interesting', text: 'Github' },
+            { href: 'https://pypi.org/project/interesting', text: 'PyPi' }
+        ]
+    },
+    {
+        thumbnail: require('./../../images/retirement-in-italy.png'),
+        altText: 'Retirement in Italy',
+        title: 'Streamlit App with Fiscal Benefit Map',
+        description: `Streamlit is a very power data visualization tool, that expedites
+            building apps with minimal infrastructure requirements. In this project,
+            I use Streamlit to graphically identify which Italian municipalities
+            offer fiscal benefits for incoming retirees. This project leverages
+            knowledge of Technology, Data, Fiscal Optimization, and Italy.`,
+        actions: [
+            { href: 'https://github.com/thegitofdaniel/retirement_in_italy', text: 'Github' },
+            { href: 'https://thegitofdaniel.github.io/retirement_in_italy', text: 'Web Deployment' },
+            { href: 'https://retirementinitaly.streamlit.app/', text: 'Streamlit Deployment' }
+        ]
+    },
+    {
+        thumbnail: require('./../../images/thewebsiteofdaniel.png'),
+        altText: 'The Website of Daniel',
+        title: 'My Website',
+        description: `Yes, this website is available on my GitHub. It has been built from
+            open-sourced templates. You may copy it too, as long as you keep the
+            license.`,
+        actions: [
+            { href: 'https://github.com/thegitofdaniel/thewebsiteofdaniel', text: 'Github' }
+        ]
+    }
+];
 
 function Portfolio() {
     return (
@@ -9,64 +48,22 @@ function Portfolio() {
             <h1>Portfolio</h1>
             <div className="container">
                 <div className="projects">
-                    <div className="project">
-                        <div className="thumbnail">
-                            <img src={pypi_interesting} alt="PyPI Interesting" />
+                    {projects.map((project, index) => (
+                        <div className="project" key={index}>
+                            <div className="thumbnail">
+                                <img src={project.thumbnail} alt={project.altText} />
+                            </div>
+                            <h2>{project.title}</h2>
+                            <p>{project.description}</p>
+                            <div className="action">
+                                {project.actions.map((action, idx) => (
+                                    <a key={idx} href={action.href}>
+                                        {action.text}
+                                    </a>
+                                ))}
+                            </div>
                         </div>
-                        <h2>interesting</h2>
-                        <p>
-                            The interesting Python package is a specialized toolkit for dealing with
-                            Brazilian fixed income titles. It provides functionalities to build,
-                            analyze, visualize, and valuate various types of fixed income
-                            instruments and portfolios made thereof.
-                        </p>
-                        <div className="action">
-                            <a href="https://github.com/thegitofdaniel/interesting">Github</a>
-                            <a href="https://pypi.org/project/interesting">PyPi</a>
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <div className="thumbnail">
-                            <img src={retirement_in_italy} alt="Retirement in Italy" />
-                        </div>
-                        <h2>Streamlit App with Fiscal Benefit Map</h2>
-                        <p>
-                            Streamlit is a very power data visualization tool, that expedites
-                            building apps with minimal infrastructure requirements. In this project,
-                            I use Streamlit to graphically identify which Italian municipalities
-                            offer fiscal benefits for incoming retirees. This project leverages
-                            knowledge of Technology, Data, Fiscal Optimization, and Italy.
-                        </p>
-                        <div className="action">
-                            <a href="https://github.com/thegitofdaniel/retirement_in_italy">
-                                Github
-                            </a>
-                            <a href="https://thegitofdaniel.github.io/retirement_in_italy">
-                                Web Deployment
-                            </a>
-                            <a href="https://retirementinitaly.streamlit.app/">
-                                Streamlit Deployment
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="project">
-                        <div className="thumbnail">
-                            <img src={the_website_of_daniel} alt="The Website of Daniel" />
-                        </div>
-                        <h2>My Website</h2>
-                        <p>
-                            Yes, this website is available on my GitHub. It has been built from
-                            open-sourced templates. You may copy it too, as long as you keep the
-                            license.
-                        </p>
-                        <div className="action">
-                            <a href="https://github.com/thegitofdaniel/thewebsiteofdaniel">
-                                Github
-                            </a>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
