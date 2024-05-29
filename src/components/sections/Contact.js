6874e5e66e9f80af3c32462b1a80190ee0d3aa70
Daniel Ruiz <43095611+thegitofdaniel@users.noreply.github.com>
2024-05-29 19:31:53 +0000

    Refactor form to controlled form

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,34 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from '@formspree/react';
 
 function Contact() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name.toLowerCase()]: value
+        });
+    };
+
+    const [state, handleSubmit] = useForm("xoqgwwlw"); // Use your Formspree form ID here
+
+    const handleFormSubmit = async (e) => {
+        await handleSubmit(e);
+        if (state.succeeded) {
+            alert('Your form was submitted succesfully. I will get back to you as soon as possible.');
+            setFormData({
+                name: '',
+                email: '',
+                message: ''
+            });
+        }
+    };
+
     return (
         <section className="contact" id="contact">
             <div className="overlay">
                 <div className="container">
                     <h1>Let&apos;s Talk</h1>
                     <p>I will get back to you as soon as possible.</p>
-                    <form action="https://formspree.io/f/xoqgwwlw" method="POST">
+                    <form onSubmit={handleFormSubmit}>
                         <input
                             type="text"
                             name="Name"
                             placeholder="Name"
                             autoComplete="name"
-                            required=""
+                            required
+                            value={formData.name}
+                            onChange={handleChange}
                         />
                         <input
                             type="email"
                             name="Email"
                             placeholder="Email"
                             autoComplete="email"
-                            required=""
+                            required
+                            value={formData.email}
+                            onChange={handleChange}
                         />
                         <textarea
                             name="Message"
                             placeholder="Message"
                             autoComplete="off"
-                            required=""
+                            required
+                            value={formData.message}
+                            onChange={handleChange}
                         ></textarea>
-                        <input type="submit" value="Submit" className="btn btn-primary" />
+                        <button type="submit" disabled={state.submitting} className="btn btn-primary">
+                            Submit
+                        </button>
                     </form>
                 </div>
             </div>
