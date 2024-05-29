44124582652093cf850bd79283406adc080dac45
Daniel Ruiz <43095611+thegitofdaniel@users.noreply.github.com>
2024-05-29 19:37:48 +0000

    Carve-out intro paragraph in Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,24 +2,25 @@ import React from 'react';
 import downloadCV from './../functions/downloadCV';
 
 function Info() {
+
+    const introduction = `As a Machine Learning Engineer, my passion lies in harnessing technology to
+    create tangible value and solve real-world challenges. I have led companies
+    in their digital transformation, end-to-end from project inception to
+    execution and hand-off. Join me in exploring the curious fields of AIML and
+    MLOPS, where complex problems meet cutting-edge solutions.`;
+
     return (
         <div className="info">
             <h1>Senior Consultant in Data &amp; AI</h1>
             <h3>Hi, I&apos;m Daniel Ruiz</h3>
-            <p>
-                As a Machine Learning Engineer, my passion lies in harnessing technology to
-                create tangible value and solve real-world challenges. I have led companies
-                in their digital transformation, end-to-end from project inception to
-                execution and hand-off. Join me in exploring the curious fields of AIML and
-                MLOPS, where complex problems meet cutting-edge solutions.
-            </p>
+            <p>{introduction}</p>
             <a className="btn btn-primary" href="#contact">
                 Let&apos;s Talk
             </a>
-            <a className="btn btn-primary" onClick={downloadCV}>
+            <button className="btn btn-primary" onClick={downloadCV}>
                 <i className="uil uil-download-alt"></i>
                 Download CV
-            </a>
+            </button>
         </div>
     );
 }
