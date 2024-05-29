0092c4ed06dea0a73e23f8a1e05fdc9bc9e5d379
Daniel Ruiz <43095611+thegitofdaniel@users.noreply.github.com>
2024-05-29 20:14:51 +0000

    Clean-up contact

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,36 +1,7 @@
-import React, { useState } from 'react';
-import { useForm } from '@formspree/react';
+import React from 'react';
 import ContactForm from './../ContactForm';
 
 function Contact() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name.toLowerCase()]: value
-        });
-    };
-
-    const [state, handleSubmit] = useForm("xoqgwwlw"); // Use your Formspree form ID here
-
-    const handleFormSubmit = async (e) => {
-        await handleSubmit(e);
-        if (state.succeeded) {
-            alert('Your form was submitted succesfully. I will get back to you as soon as possible.');
-            setFormData({
-                name: '',
-                email: '',
-                message: ''
-            });
-        }
-    };
-
     return (
         <section className="contact" id="contact">
             <div className="overlay">
